49f2263c88eec6cda890cec4d47bf46438c02ba5
Rutvik_surani <Rutviksurani999@gmail.com>
2022-02-25 12:19:17 +0530

    setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <b-autocomplete
+      rounded
+      v-model="name"
+      :data="filteredDataArray"
+      placeholder="e.g. jQuery"
+      icon="magnify"
+      clearable
+    />
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+export default Vue.extend({
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+});
+</script>
