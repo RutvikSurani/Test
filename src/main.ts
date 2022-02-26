ff380c24e622e66cad9e58c7b6a7f8cddc71fc80
Rutvik_surani <Rutviksurani999@gmail.com>
2022-02-26 13:44:09 +0530

    chart done

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Buefy from "buefy";
+import Highcharts from "highcharts";
+import Stock from "highcharts/modules/stock";
+import HighchartsVue from "highcharts-vue";
 
+Stock(Highcharts);
 Vue.config.productionTip = false;
+Vue.use(Buefy);
+Vue.use(HighchartsVue);
 
 new Vue({
   router,
